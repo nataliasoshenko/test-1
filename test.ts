a18968748e645f65c94a8325d71adafcfc49c2f5
nataliasoshenko <151614890+nataliasoshenko@users.noreply.github.com>
2024-05-16 13:21:10 +0400

    dc

--- test.ts
+++ test.ts
@@ -1,6 +1,3 @@
 let divElement = document.createElement("div");
 const tempDiv = document.createElement("div");
 
-if (divElement) {
-    divElement.insertAdjacentElement("beforebegin", tempDiv);
-}
